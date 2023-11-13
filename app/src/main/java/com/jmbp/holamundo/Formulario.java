6d0a2f5e05d10fdf897473baf7debe3e4c7d0464
MiguelBrucil <132540041+MiguelBrucil@users.noreply.github.com>
2023-11-13 16:41:04 -0500

    Creacion de ingreso de botones con sus respectivo estado

--- app/src/main/java/com/jmbp/holamundo/Formulario.java
+++ app/src/main/java/com/jmbp/holamundo/Formulario.java
@@ -3,6 +3,10 @@ package com.jmbp.holamundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        //crear objetos layout
+        Button btnValidar=findViewById(R.id.btnValidar);
+        EditText edNombre,edApellidos,edCedula,edCorreo,edDireccion;
+        edNombre=findViewById(R.id.txtNombre);
+        edApellidos=findViewById(R.id.txtApellido);
+        edCedula=findViewById(R.id.txtCedula);
+        edCorreo=findViewById(R.id.txtCorreo);
+        edDireccion=findViewById(R.id.txtDireccion);
+
+        btnValidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre=edNombre.getText().toString();
+                if(!nombre.isEmpty()){
+                    Toast.makeText(Formulario.this,"Informacion de ingreso incorrecto",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
